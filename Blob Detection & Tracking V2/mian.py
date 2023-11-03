e33b32288af8f2b4e3ca6d61301a2a11b29a1b10
Hanqing Qi <billy.qi03@gmail.com>
2023-11-03 19:02:19 -0400

    🎉 Start `main.py` & ✅ Passed Class Import Test
    Start working on new main python file, openmv ide's class import is tested and working.

---
+++ Blob Detection & Tracking V2/mian.py
@@ -0,0 +1,8 @@
+"""
+Author       : Hanqing Qi
+Date         : 2023-11-03 18:53:44
+LastEditors  : Hanqing Qi
+LastEditTime : 2023-11-03 19:00:03
+FilePath     : /Bicopter-Vision-Control/Blob Detection & Tracking V2/mian.py
+Description  : The main python file for blob detection and tracking running on Nicla Vision
+"""
